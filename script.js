2629bb3e4279b0db0012b3a793e7200ec9877c12
Adam Sebesta <adam.sebesta5@gmail.com>
2021-05-13 17:11:17 +0200

    added new mp4s, fixed links

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+WINDOWWIDTH = 0;
+
 window.addEventListener('load', (event) => {
   scrollPageToTop();
   initPageScroll();
   initBurgerMenu();
+  refreshPageONMobileResize();
+  WINDOWWIDTH = window.innerWidth
 });
 
 const initPageScroll = () => {
@@ -11,108 +15,62 @@ const initPageScroll = () => {
     scdir, hold = false;
 
   function _scrollY(obj) {
-    var slength, plength, pan, step = 100,
-      vh = window.innerHeight / 100,
-      vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
-    if ((this !== undefined && this.id === 'container') || (obj !== undefined && obj.id === 'container')) {
-      pan = this || obj;
-      plength = parseInt(pan.offsetHeight / vh);
-    }
-    if (pan === undefined) {
-      return;
-    }
-    plength = plength || parseInt(pan.offsetHeight / vmin);
-    slength = parseInt(pan.style.transform.replace('translateY(', ''));
-    if (scdir === 'up' && Math.abs(slength) < (plength - plength / pnls)) {
-      slength = slength - step;
-    } else if (scdir === 'down' && slength < 0) {
-      slength = slength + step;
-    } else if (scdir === 'top') {
-      slength = 0;
-    }
-    if (hold === false) {
-      hold = true;
-      pan.style.transform = 'translateY(' + slength + 'vh)';
-      setTimeout(function () {
-        hold = false;
-      }, 1000);
+
+    if (window.innerWidth >= 1100) {
+
+      var slength, plength, pan, step = 100,
+        vh = window.innerHeight / 100,
+        vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
+      if ((this !== undefined && this.id === 'container') || (obj !== undefined && obj.id === 'container')) {
+        pan = this || obj;
+        plength = parseInt(pan.offsetHeight / vh);
+      }
+      if (pan === undefined) {
+        return;
+      }
+      plength = plength || parseInt(pan.offsetHeight / vmin);
+      slength = parseInt(pan.style.transform.replace('translateY(', ''));
+      if (scdir === 'up' && Math.abs(slength) < (plength - plength / pnls)) {
+        slength = slength - step;
+      } else if (scdir === 'down' && slength < 0) {
+        slength = slength + step;
+      } else if (scdir === 'top') {
+        slength = 0;
+      }
+      if (hold === false) {
+        hold = true;
+        pan.style.transform = 'translateY(' + slength + 'vh)';
+        setTimeout(function () {
+          hold = false;
+        }, 1000);
+      }
     }
   }
 
-  /*[swipe detection on touchscreen devices - if you want it]*/
-  // function _swipe(obj) {
-  //   var swdir,
-  //     sX,
-  //     sY,
-  //     dX,
-  //     dY,
-  //     threshold = 100,
-  //     /*[min distance traveled to be considered swipe]*/
-  //     slack = 50,
-  //     /*[max distance allowed at the same time in perpendicular direction]*/
-  //     alT = 500,
-  //     /*[max time allowed to travel that distance]*/
-  //     elT, /*[elapsed time]*/
-  //     stT; /*[start time]*/
-  //   obj.addEventListener('touchstart', function (e) {
-  //     var tchs = e.changedTouches[0];
-  //     swdir = 'none';
-  //     sX = tchs.pageX;
-  //     sY = tchs.pageY;
-  //     stT = new Date().getTime();
-  //     //e.preventDefault();
-  //   }, false);
-
-  //   obj.addEventListener('touchmove', function (e) {
-  //     e.preventDefault(); /*[prevent scrolling when inside DIV]*/
-  //   }, false);
-
-  //   obj.addEventListener('touchend', function (e) {
-  //     var tchs = e.changedTouches[0];
-  //     dX = tchs.pageX - sX;
-  //     dY = tchs.pageY - sY;
-  //     elT = new Date().getTime() - stT;
-  //     if (elT <= alT) {
-  //       if (Math.abs(dX) >= threshold && Math.abs(dY) <= slack) {
-  //         swdir = (dX < 0) ? 'left' : 'right';
-  //       } else if (Math.abs(dY) >= threshold && Math.abs(dX) <= slack) {
-  //         swdir = (dY < 0) ? 'up' : 'down';
-  //       }
-  //       if (obj.id === 'container') {
-  //         if (swdir === 'up') {
-  //           scdir = swdir;
-  //           _scrollY(obj);
-  //         } else if (swdir === 'down' && obj.style.transform !== 'translateY(0)') {
-  //           scdir = swdir;
-  //           _scrollY(obj);
-
-  //         }
-  //         e.stopPropagation();
-  //       }
-  //     }
-  //   }, false);
-  // }
   /*[assignments]*/
   var container = document.querySelector('.container');
   container.style.transform = 'translateY(0)';
   container.addEventListener('wheel', function (e) {
-    if (e.deltaY < 0) {
-      scdir = 'down';
+    if (window.innerWidth >= 1100) {
+      if (e.deltaY < 0) {
+        scdir = 'down';
+      }
+      if (e.deltaY > 0) {
+        scdir = 'up';
+      }
+      e.stopPropagation();
     }
-    if (e.deltaY > 0) {
-      scdir = 'up';
-    }
-    e.stopPropagation();
   });
   container.addEventListener('wheel', _scrollY);
-  // _swipe(container);
-  var tops = document.querySelectorAll('.top');
-  for (var i = 0; i < tops.length; i++) {
-    tops[i].addEventListener('click', function () {
-      scdir = 'top';
-      _scrollY(container);
-    });
-  }
+}
+
+const refreshPageONMobileResize = () => {
+  window.addEventListener('resize', (e) => {
+    WINDOWWIDTH = e.target.innerWidth;
+    if (window.innerWidth > 1100 && WINDOWWIDTH < 1100) {
+      window.refresh();
+    }
+  });
 }
 
 const initBurgerMenu = () => {

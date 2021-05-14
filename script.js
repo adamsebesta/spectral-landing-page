35b25dff1b44caf1af99dc96ba3a458014be9c78
Adam Sebesta <adam.sebesta5@gmail.com>
2021-05-14 16:46:55 +0200

    updated background color

--- script.js
+++ script.js
@@ -14,190 +14,191 @@ let scrollTopBtn = document.querySelector('#scroll-top');
 
 
 window.addEventListener('load', (event) => {
-  scrollPageToTop();
-  initPageScroll();
-  initBurgerMenu();
-  initModal();
-  initScrollTopButton();
-  resetFormOnSubmit();
-});
+      scrollPageToTop();
+      initPageScroll();
+      initBurgerMenu();
+      initModal();
+      initScrollTopButton();
+      resetFormOnSubmit();
 
-const initPageScroll = () => {
-  "use strict";
-  /*[pan and container CSS scrolls]*/
-  var pnls = document.querySelectorAll('.section').length,
-    scdir, hold = false;
+      // 
 
-  function _scrollY(obj) {
+      const initPageScroll = () => {
+        "use strict";
+        /*[pan and container CSS scrolls]*/
+        var pnls = document.querySelectorAll('.section').length,
+          scdir, hold = false;
 
-    if (window.innerWidth >= 1100 && modal.style.display != 'flex') {
+        function _scrollY(obj) {
 
-      var slength, plength, pan, step = 100,
-        vh = window.innerHeight / 100,
-        vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
-      if ((this !== undefined && this.id === 'container') || (obj !== undefined && obj.id === 'container')) {
-        pan = this || obj;
-        plength = parseInt(pan.offsetHeight / vh);
-      }
-      if (pan === undefined) {
-        return;
-      }
-      plength = plength || parseInt(pan.offsetHeight / vmin);
-      slength = parseInt(pan.style.transform.replace('translateY(', ''));
-      if (scdir === 'up' && Math.abs(slength) < (plength - plength / pnls)) {
-        slength = slength - step;
-      } else if (scdir === 'down' && slength < 0) {
-        slength = slength + step;
-      } else if (scdir === 'top') {
-        slength = 0;
-      }
-      if (hold === false) {
-        hold = true;
-        pan.style.transform = 'translateY(' + slength + 'vh)';
-        setTimeout(function () {
-          hold = false;
-        }, 1000);
-      }
-    }
-  }
+          if (window.innerWidth >= 1100 && modal.style.display != 'flex') {
 
-  /*[assignments]*/
-  var container = document.querySelector('.container');
-  container.style.transform = 'translateY(0)';
-  container.addEventListener('wheel', function (e) {
-    if (window.innerWidth >= 1100) {
-      if (e.deltaY < 0) {
-        scdir = 'down';
-      }
-      if (e.deltaY > 0) {
-        scdir = 'up';
-      }
-      e.stopPropagation();
-    }
-  });
-  container.addEventListener('wheel', _scrollY);
-}
+            var slength, plength, pan, step = 100,
+              vh = window.innerHeight / 100,
+              vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
+            if ((this !== undefined && this.id === 'container') || (obj !== undefined && obj.id === 'container')) {
+              pan = this || obj;
+              plength = parseInt(pan.offsetHeight / vh);
+            }
+            if (pan === undefined) {
+              return;
+            }
+            plength = plength || parseInt(pan.offsetHeight / vmin);
+            slength = parseInt(pan.style.transform.replace('translateY(', ''));
+            if (scdir === 'up' && Math.abs(slength) < (plength - plength / pnls)) {
+              slength = slength - step;
+            } else if (scdir === 'down' && slength < 0) {
+              slength = slength + step;
+            } else if (scdir === 'top') {
+              slength = 0;
+            }
+            if (hold === false) {
+              hold = true;
+              pan.style.transform = 'translateY(' + slength + 'vh)';
+              setTimeout(function () {
+                hold = false;
+              }, 1000);
+            }
+          }
+        }
 
-const initModal = () => {
-  if (modal && backBtn && appBtn) {
-    appBtn.addEventListener('click', (e) => {
-      modal.style.display = 'flex';
-      if (window.innerWidth <= 1100) {
-        modalContent.classList.add('modal-full-page');
-        body.classList.add('stop-scroll');
+        /*[assignments]*/
+        var container = document.querySelector('.container');
+        container.style.transform = 'translateY(0)';
+        container.addEventListener('wheel', function (e) {
+          if (window.innerWidth >= 1100) {
+            if (e.deltaY < 0) {
+              scdir = 'down';
+            }
+            if (e.deltaY > 0) {
+              scdir = 'up';
+            }
+            e.stopPropagation();
+          }
+        });
+        container.addEventListener('wheel', _scrollY);
       }
-    })
-    backBtn.addEventListener('click', (e) => {
-      modal.style.display = 'none';
-      if (window.innerWidth <= 1100) {
-        body.classList.remove('stop-scroll');
-      }
-    })
-    // close modal on blurry click
-    modal.addEventListener('click', (e) => {
-      if (window.innerWidth >= 1100) {
-        if (e.target == modal) {
-          modal.style.display = 'none';
+
+      const initModal = () => {
+        if (modal && backBtn && appBtn) {
+          appBtn.addEventListener('click', (e) => {
+            modal.style.display = 'flex';
+            if (window.innerWidth <= 1100) {
+              modalContent.classList.add('modal-full-page');
+              body.classList.add('stop-scroll');
+            }
+          })
+          backBtn.addEventListener('click', (e) => {
+            modal.style.display = 'none';
+            if (window.innerWidth <= 1100) {
+              body.classList.remove('stop-scroll');
+            }
+          })
+          // close modal on blurry click
+          modal.addEventListener('click', (e) => {
+            if (window.innerWidth >= 1100) {
+              if (e.target == modal) {
+                modal.style.display = 'none';
+              }
+            }
+          })
         }
       }
-    })
-  }
-}
 
-const initBurgerMenu = () => {
-  if (window.innerWidth >= 1100) {
-    desktopToggleMenu();
-  } else {
-    mobileToggleMenu();
-  }
-}
-
-const desktopToggleMenu = () => {
-  if (burger) {
-    burger.addEventListener('click', (e) => {
-      if (burgerIcon.classList.contains('open')) {
-        burgerIcon.classList.remove('open');
-        // slight delay when bringing 'app' button back
-        setTimeout(() => {
-          appBtnWrapper.style.display = "";
-        }, 150)
-
-        navMenu.style.transform = "translateX(120%)";
-
-      } else {
-        burgerIcon.classList.add('open');
-        appBtnWrapper.style.display = "none"
-        navMenu.style.visibility = "visible";
-        navMenu.style.transform = "translateX(0)";
-      }
-    })
-  }
-}
-
-const mobileToggleMenu = () => {
-  if (burger) {
-    burger.addEventListener('touchstart', (e) => {
-      if (burgerIcon.classList.contains('open')) {
-        // close menu
-        burgerIcon.classList.remove('open');
-        // slight delay when bringing 'app' button back
-        setTimeout(() => {
-          appBtnWrapper.style.display = "";
-        }, 100)
-        body.classList.remove('stop-scroll');
-        mobileNavMenu.style.transform = "translateY(-100%)";
-
-      } else {
-        // open menu
-        burgerIcon.classList.add('open');
-        appBtnWrapper.style.display = "none"
-        // inhibit scroll
-        body.classList.add('stop-scroll');
-        mobileNavMenu.style.visibility = "visible";
-        mobileNavMenu.style.transform = "translateY(0)";
-      }
-    })
-  }
-}
-
-
-const scrollPageToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  })
-};
-
-const resetFormOnSubmit = () => {
-  if (emailForm && submitBtn) {
-    submitBtn.addEventListener('click', () => {
-      setTimeout(() => {
-        emailForm.reset();
-      }, 1000)
-    })
-  }
-}
-
-const initScrollTopButton = () => {
-  container.addEventListener('wheel', () => {
-    // if not on page 1
-    setTimeout(() => {
-      if (window.innerWidth >= 1100) {
-        if (container.style.transform != 'translateY(0vh)') {
-          scrollTopBtn.style.display = "block"
+      const initBurgerMenu = () => {
+        if (window.innerWidth >= 1100) {
+          desktopToggleMenu();
         } else {
-          scrollTopBtn.style.display = "none"
+          mobileToggleMenu();
         }
       }
-    }, 200)
-  })
 
-  if (window.innerWidth >= 1100) {
-    scrollTopBtn.addEventListener('click', () => {
-      // scroll to top and remove button
-      container.style.transform = 'translateY(0)';
-      scrollTopBtn.style.display = "none"
-    })
-  }
+      const desktopToggleMenu = () => {
+        if (burger) {
+          burger.addEventListener('click', (e) => {
+            if (burgerIcon.classList.contains('open')) {
+              burgerIcon.classList.remove('open');
+              // slight delay when bringing 'app' button back
+              setTimeout(() => {
+                appBtnWrapper.style.display = "";
+              }, 150)
 
-}
+              navMenu.style.transform = "translateX(120%)";
+
+            } else {
+              burgerIcon.classList.add('open');
+              appBtnWrapper.style.display = "none"
+              navMenu.style.visibility = "visible";
+              navMenu.style.transform = "translateX(0)";
+            }
+          })
+        }
+      }
+
+      const mobileToggleMenu = () => {
+        if (burger) {
+          burger.addEventListener('touchstart', (e) => {
+            if (burgerIcon.classList.contains('open')) {
+              // close menu
+              burgerIcon.classList.remove('open');
+              // slight delay when bringing 'app' button back
+              setTimeout(() => {
+                appBtnWrapper.style.display = "";
+              }, 100)
+              body.classList.remove('stop-scroll');
+              mobileNavMenu.style.transform = "translateY(-100%)";
+
+            } else {
+              // open menu
+              burgerIcon.classList.add('open');
+              appBtnWrapper.style.display = "none"
+              // inhibit scroll
+              body.classList.add('stop-scroll');
+              mobileNavMenu.style.visibility = "visible";
+              mobileNavMenu.style.transform = "translateY(0)";
+            }
+          })
+        }
+      }
+
+
+      const scrollPageToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        })
+      };
+
+      const resetFormOnSubmit = () => {
+        if (emailForm && submitBtn) {
+          submitBtn.addEventListener('click', () => {
+            setTimeout(() => {
+              emailForm.reset();
+            }, 1000)
+          })
+        }
+      }
+
+      const initScrollTopButton = () => {
+        container.addEventListener('wheel', () => {
+          // if not on page 1
+          setTimeout(() => {
+            if (window.innerWidth >= 1100) {
+              if (container.style.transform != 'translateY(0vh)') {
+                scrollTopBtn.style.display = "block"
+              } else {
+                scrollTopBtn.style.display = "none"
+              }
+            }
+          }, 200)
+        })
+
+        if (window.innerWidth >= 1100) {
+          scrollTopBtn.addEventListener('click', () => {
+            // scroll to top and remove button
+            container.style.transform = 'translateY(0)';
+            scrollTopBtn.style.display = "none"
+          })
+        }
+
+      }
